1ce83f24e5808e529dfd30628f62ec192deb8ebc
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2025-02-23 16:41:02 +0530

    updated package file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,13 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
-import Navbar from "./layout/Navbar";
-import Dashboard from "./views/dashboard/Dashboard";
-import Projects from "./views/admin/Projects";
-import AdminNavbar from "./layout/AdminNavbar";
-import AssignProjects from "./views/users/AssignProjects";
-import TaskManagement from "./views/users/TaskManagement";
-import AssignTask from "./views/users/AssignTask";
 import Login from "./authentication/Login";
-import Register from "./authentication/Register";
-import TextArea from "./components/forms/formComponents/TextArea";
-import Report from "./views/admin/Report";
+
 
 function App() {
   return (
     <div>
-
+<h1> hello world</h1>
+<Login/>
     </div>
   );
 }
