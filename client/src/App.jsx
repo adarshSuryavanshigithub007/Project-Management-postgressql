6f79b8e719d8be4046c72fe223714121a79cd533
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2025-03-14 22:16:02 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,33 +8,33 @@ import { protectedRoutes } from "./routes/ProtectedRoutes";
 import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
-  const token = getToken();
-  console.log(token)
-  return (
-    <HashRouter>
-      <ToastContainer />
-      <Routes>
-        {publicRoutes.map((route, i) => (
-          <Route path={route.path} element={route.element} key={i} />
-        ))}
-        <Route
-          path="/"
-          element={
-            token ? <Navigate to="/dashboard" /> : <Navigate to="/auth/login" />
-          }
-        />
-        <Route path="/" element={<Main />}>
-          {protectedRoutes.map((route, i) => (
-            <Route
-              path={route.path}
-              element={<PrivateRoute element={route.element} />}
-              key={i}
-            />
-          ))}
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
+    const token = getToken();
+    console.log(token)
+    return (
+        <HashRouter>
+            <ToastContainer />
+            <Routes>
+                {publicRoutes.map((route, i) => (
+                    <Route path={route.path} element={route.element} key={i} />
+                ))}
+                <Route
+                    path="/"
+                    element={
+                        token ? <Navigate to="/dashboard" /> : <Navigate to="/auth/login" />
+                    }
+                />
+                <Route path="/" element={<Main />}>
+                    {protectedRoutes.map((route, i) => (
+                        <Route
+                            path={route.path}
+                            element={<PrivateRoute element={route.element} />}
+                            key={i}
+                        />
+                    ))}
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
 };
 
 export default App;
